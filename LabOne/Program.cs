1b1c1d6890c0cfd8257b7d2f01be2e03217783df
Vladimir Monomakh <56538059+POSE1D0N-AP@users.noreply.github.com>
2021-03-02 20:14:34 +0600

    lab week 6

--- LabOne/Program.cs
+++ LabOne/Program.cs
@@ -5,54 +5,132 @@ using System.Linq;
 
 namespace LabOne
 {
-    class Program
+ class Program
     {
         static void Main(string[] args)
         {
-            Car myFirstCar = new Car();
-            Car mySecondCar = new Car(13);
+            God newGod = God.GetGod(false);
+            Animal myAnimal = new Animal("dog", 4, false);
+            Human jack = new Human("Jack Doe", "primacy", true);
 
-            myFirstCar.CarId = 14;
-            mySecondCar.editCarId(66);
+            myAnimal.Resurrect();
+            jack.Kill();
+            jack.Name = "Jonh Doe";
 
-            Console.WriteLine(myFirstCar.editCarId());
-            Console.WriteLine(mySecondCar.editCarId());
 
+            Console.WriteLine(newGod.GetInfo());
+            Console.WriteLine(myAnimal.GetInfo());
+            Console.WriteLine(jack.GetInfo());
         }
     }
 
-    class Car
+
+    class God
     {
-        private int carId;
-        public int CarId
+        private float researchPercentage;
+        protected bool isLive;
+        protected bool IsLifeSet
         {
-            get 
-            { 
-                return carId; 
-            }   
-            set 
-            { 
-                carId = value; 
-            }  
+            set { isLive = value; }
         }
 
-        public string IDKWhyThisNeeded
-        { get; set; }
-
-        public Car() { }
-        public Car(int carId)
+        public bool IsLifeGet
         {
-            this.carId = carId;
-        }
-    
-        public string editCarId()
-        {
-            return ("#" + carId.ToString());
+            get { return isLive; }
         }
 
-        public void editCarId(int addToId)
+        protected God(bool isLive) 
         {
-            carId += addToId;
+            this.isLive = isLive;
+        }
+
+        private static God godInstance;
+        public static God GetGod(bool isLive)
+        {
+            if (godInstance == null)
+            {
+                godInstance = new God(isLive);
+            }
+            return godInstance;
+        }
+
+        public string GetInfo()
+        {
+            if (isLive)
+            {
+                return "God is alive!";
+            }
+            else
+            {
+                return "God is Die!";
+            }
+        }
+    }
+
+    class Animal : God
+    {
+        private int legs;
+        public int Legs
+        {
+            set { legs = value; }
+            get { return legs; }
+        }
+        protected string animalKingdom;
+        public string AnimalKingdom
+        {
+            set { animalKingdom = value; }
+            get { return animalKingdom; }
+        }
+        public Animal(string animalKingdom, int legs, bool isLive) : base(isLive) 
+        {
+            this.AnimalKingdom = animalKingdom;
+            this.legs = legs;
+        }
+        protected Animal(string animalKingdom, bool isLive) : base(isLive)
+        {
+            this.AnimalKingdom = animalKingdom;
+        }
+
+        public void Kill()
+        {
+            if (isLive)
+            {
+                isLive = false;
+            }
+        }
+        public void Resurrect()
+        {
+            if (!isLive)
+            {
+                isLive = true;
+            }
+        }
+
+        public new string GetInfo()
+        {
+            string res = animalKingdom + " " + legs + " " + (isLive ? "animal is alive." : "animal is dead.");
+            return res; 
+        }
+    }
+
+    class Human : Animal
+    {
+        private string name;
+        public string Name
+        {
+            set { name = value; }
+            get { return name; }
+        }
+
+        public Human(string name, string animalKingdom, bool isLive) : base(animalKingdom, isLive)
+        {
+            this.name = name;
+        }
+
+        public new string GetInfo()
+        {
+            string res = name + " is " + animalKingdom + " and  " + (isLive ? "is alive." : "is dead.");
+            return res;
         }
     }
 }
